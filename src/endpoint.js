8ac9d9908321dde3f74cdd89537963c57189d39e
Zachary Godfrey <ZacharyGodfrey@users.noreply.github.com>
2022-01-20 16:26:33 -0500

    Extract endpoint logic to separate file

---
+++ src/endpoint.js
@@ -0,0 +1,54 @@
+const { http } = require('./util');
+const actions = require('./actions');
+
+const handleRequest = async (db, action, request) => {
+    try {
+        if (action.authenticate) {
+            if (!request.token) {
+                return http.response.badRequest(['Token Required']);
+            } else {
+                request.user = await db.userByToken(request.token);
+
+                if (!request.user) {
+                    return http.response.badRequest(['Invalid Token']);
+                }
+            }
+        }
+
+        const errors = await action.validate(db, request);
+
+        if (errors.length > 0) {
+            return http.response.badRequest(errors);
+        }
+
+        const result = await action.execute(db, request);
+
+        return http.response.success(result);
+    } catch (error) {
+        return http.response.serverError(error.message);
+    }
+};
+
+module.exports = (db) => {
+    return async (req, res, next) => {
+        const action = actions[req.params.actionName];
+
+        if (!action) {
+            next();
+        } else {
+            const request = http.request.fromExpress(req);
+
+            if (action.preHandler) {
+                await action.preHandler(db, request);
+            }
+
+            const response = await handleRequest(db, action, request);
+
+            if (action.postHandler) {
+                await action.postHandler(db, request, response);
+            }
+
+            res.status(response.status).json(response.body);
+        }
+    };
+};
--- src/server.js
+++ src/server.js
@@ -1,90 +1,42 @@
 const express = require('express');
 
 const { http } = require('./util');
-const actions = require('./actions');
-
-const handleRequest = async (db, action, request) => {
-  try {
-    if (action.authenticate) {
-      if (!request.token) {
-        return http.response.badRequest(['Token Required']);
-      } else {
-        request.user = await db.userByToken(request.token);
-
-        if (!request.user) {
-          return http.response.forbidden();
-        }
-      }
-    }
-
-    const errors = await action.validate(db, request);
-
-    if (errors.length > 0) {
-      return http.response.badRequest(errors);
-    }
-
-    const result = await action.execute(db, request);
-
-    return http.response.success(result);
-  } catch (error) {
-    return http.response.serverError(error.message);
-  }
-};
+const endpoint = require('./endpoint');
 
 module.exports = (db) => {
-  const server = express();
+    const server = express();
 
-  server.set('json spaces', 2);
-  server.use(express.urlencoded({ extended: false }));
-  server.use(express.json());
+    server.set('json spaces', 2);
+    server.use(express.urlencoded({ extended: false }));
+    server.use(express.json());
 
-  // CORS
-  server.use((req, res, next) => {
-    // Allow CORS
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    // CORS
+    server.use((req, res, next) => {
+        // Allow CORS
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST');
 
-    // Remove headers that leak information
-    res.removeHeader('x-powered-by');
+        // Remove headers that leak information
+        res.removeHeader('x-powered-by');
 
-    // Handle OPTIONS requests
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-    } else {
-      next();
-    }
-  });
+        // Handle OPTIONS requests
+        if (req.method === 'OPTIONS') {
+            res.status(200).end();
+        } else {
+            next();
+        }
+    });
 
-  // Endpoints
-  server.post('/:actionName', async (req, res) => {
-    const action = actions[req.params.actionName];
+    // Request Handler
+    server.post('/:actionName', endpoint(db));
 
-    if (!action) {
-      next();
-    } else {
-      const request = http.request.fromExpress(req);
+    // 404
+    server.use((req, res) => {
+        const r = http.response.notFound();
 
-      if (action.preHandler) {
-        await action.preHandler(db, request);
-      }
+        res.status(r.status).json(r.body);
+    });
 
-      const response = await handleRequest(db, action, request);
-
-      if (action.postHandler) {
-        await action.postHandler(db, request, response);
-      }
-
-      res.status(response.status).json(response.body);
-    }
-  });
-
-  // 404
-  server.use((req, res) => {
-    const r = http.response.notFound();
-
-    res.status(r.status).json(r.body);
-  });
-
-  return server;
+    return server;
 };
