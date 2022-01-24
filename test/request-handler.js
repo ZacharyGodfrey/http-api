b9af44bc6f501d4855df6fe6ba331d26bbde76d3
Zachary Godfrey <ZacharyGodfrey@users.noreply.github.com>
2022-01-24 13:59:16 -0500

    Add tests

--- test/request-handler.js
+++ test/request-handler.js
@@ -1,4 +1,6 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
+const sandbox = sinon.createSandbox();
 
 const createHandler = require('../src/request-handler');
 
@@ -15,16 +17,28 @@ const context = {
 	http: require('../src/http')(env)
 };
 
-const action = {
-	name: 'test-action',
-	authenticate: false,
-	validate: async (db, request) => ([]),
-	execute: async (db, request) => ({ ...request })
+const actions = {
+	'test-auth': {
+		name: 'test-auth',
+		authenticate: true,
+		validate: async (db, request) => ([]),
+		execute: async (db, request) => ({ ...request })
+	},
+	'test-no-auth': {
+		name: 'test-no-auth',
+		authenticate: false,
+		validate: async (db, request) => ([]),
+		execute: async (db, request) => ({ ...request })
+	}
 };
 
 const handleRequest = createHandler(context, [action]);
 
 describe('src/request-handler.js', () => {
+	afterEach(() => {
+		sandbox.restore();
+	});
+
 	describe('when the requested action does not exist', () => {
 		it('should return notFound()', async () => {
 			const actual = await handleRequest({
@@ -32,6 +46,73 @@ describe('src/request-handler.js', () => {
 			});
 
 			expect(actual).to.deep.eq(context.http.response.notFound());
+		});
+	});
+
+	describe('when the action requires authentication', () => {
+		describe('when the request is missing a token', () => {
+			it('should return notAuthorized()', async () => {
+				const actual = await handleRequest({
+					actionName: 'test-auth',
+					token: null
+				});
+
+				expect(actual).to.deep.eq(context.http.response.notAuthorized());
+			});
+		});
+
+		describe('when the user is not found', () => {
+			it('should return badRequest()', async () => {
+				sandbox.stub(context.db, 'userByToken').resolves(null);
+
+				const actual = await handleRequest({
+					actionName: 'test-auth',
+					token: 'fake-token'
+				});
+
+				expect(actual).to.deep.eq(context.http.response.badRequest(['Invalid Token']));
+			});
+		});
+
+		describe('when the user is found', () => {
+			it('should not return badRequest()', async () => {
+				sandbox.stub(context.db, 'userByToken').resolves(null);
+
+				const actual = await handleRequest({
+					actionName: 'test-auth',
+					token: 'fake-token'
+				});
+
+				expect(actual).to.not.deep.eq(context.http.response.badRequest(['Invalid Token']));
+			});
+		});
+	});
+
+	describe('when the request payload is not valid', () => {
+		it('should return badRequest()', async () => {
+			const errors = ['Fake Error'];
+
+			sandbox.stub(actions['test-no-auth'], 'validate').resolves(errors);
+
+			const actual = await handleRequest({
+				actionName: 'test-no-auth'
+			});
+
+			expect(actual).to.deep.eq(context.http.response.badRequest(errors));
+		});
+	});
+
+	describe('when the request payload is valid', () => {
+		it('should return success()', async () => {
+			const result = { abc: 123 };
+
+			sandbox.stub(actions['test-no-auth'], 'execute').resolves(result);
+
+			const actual = await handleRequest({
+				actionName: 'test-no-auth'
+			});
+
+			expect(actual).to.deep.eq(context.http.response.success(result));
 		});
 	});
 });
